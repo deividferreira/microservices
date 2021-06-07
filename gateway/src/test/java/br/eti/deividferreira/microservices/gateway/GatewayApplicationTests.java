8fa321a7f19768d8f2d7f61f7b71c6c5bd7291a3
Deivid Ferreira <deividpse@gmail.com>
2021-06-07 14:15:34 -0300

    init spring gateway

---
+++ gateway/src/test/java/br/eti/deividferreira/microservices/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.eti.deividferreira.microservices.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
